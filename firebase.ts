53f3b6dfc7818eeb76834326f376541b42cae14e
Le Minh Kha <lmkha0201@gmail.com>
2025-05-24 20:25:52 +0700

    refactor: reverse old firebase config

--- firebase.ts
+++ firebase.ts
@@ -2,13 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getMessaging, isSupported } from "firebase/messaging";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCh6K1dFUqfMbzA1A2jiHbCrc1aXz0yo1s",
-    authDomain: "notificationservicefcm-1e60f.firebaseapp.com",
-    projectId: "notificationservicefcm-1e60f",
-    storageBucket: "notificationservicefcm-1e60f.firebasestorage.app",
-    messagingSenderId: "989490054728",
-    appId: "1:989490054728:web:1f29bea164ad9f4218fa7d",
-    measurementId: "G-BEC4ZK8GTN"
+    apiKey: "AIzaSyCvHMFoZrZZOw9b6eBRoLOWfajUQ0eYN84",
+    authDomain: "visara-notification.firebaseapp.com",
+    projectId: "visara-notification",
+    storageBucket: "visara-notification.firebasestorage.app",
+    messagingSenderId: "281680746888",
+    appId: "1:281680746888:web:a8f670a55e99554726c23d"
 };
 
 const app = initializeApp(firebaseConfig);
